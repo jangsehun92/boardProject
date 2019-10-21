b739947a3218b9eec0d6207e3735fb7ef78fabdd
jangsehun92 <jangsehun92@gmail.com>
2019-10-21 21:25:02 +0900

    191021_인증테이블 추가 후 작업중

--- src/main/java/jsh/spring/project/domain/member/service/MemberRegisterService.java
+++ src/main/java/jsh/spring/project/domain/member/service/MemberRegisterService.java
@@ -4,11 +4,7 @@ import jsh.spring.project.domain.member.dto.RegisterConfirmRequest;
 import jsh.spring.project.domain.member.dto.RegisterRequest;
 
 public interface MemberRegisterService {
-	//회원가입 방식이 두가지(일반 회원가입, SNS 회원가입)
 	public void singUp(RegisterRequest dto) throws Exception;
-	
-	public void updateAuthStatus(RegisterConfirmRequest dto);
-	
+	public void updateStatus(RegisterConfirmRequest dto) throws Exception;
 	public void resendEmail(String email) throws Exception;
-	
 }
